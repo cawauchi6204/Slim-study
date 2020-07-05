ff0913b5c40ac713f083af0d597c5c0ab0fe67f4
cawauchi6204 <cawauchi6204@gmail.com>
2020-07-05 17:21:57 +0900

    3-1終了

--- public/index.php
+++ public/index.php
@@ -2,3 +2,17 @@
 use Slim\Factory\AppFactory;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterFace;
+
+require_once($_SERVER["DOCUMENT_ROOT"]."/firstslim/vendor/autoload.php");
+
+$app = AppFactory::create();
+
+$app->get(
+    "/firstslim/public/hello",
+    function (ServerRequestInterface $request, ResponseInterFace $response, array $args): ResponseInterFace {
+        print("Hello World");
+        return $response;
+    }
+);
+
+$app->run();
